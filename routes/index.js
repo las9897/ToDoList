1d3afe0fadb4e94029281587fe78c2a4377116b5
JeJe <las9897@gmail.com>
2019-05-19 21:40:55 +0900

    add deadline:year

--- routes/index.js
+++ routes/index.js
@@ -15,38 +15,51 @@ module.exports = function(app, Todo) {
     var todo = new Todo();
     todo.title = req.body.title;
     if (todo.title == "" || todo.title == null) {
-      res.json({ result: 0, error: "Title Valiation Error", message: "Please input title" });
+      res.json({
+        result: 0,
+        error: "Title Valiation Error",
+        message: "Please input title"
+      });
       return;
     }
     todo.content = req.body.content;
 
     if (req.body.deadline != "") {
       var split_deadline = req.body.deadline.split("-");
-      if (split_deadline.length != 2) {
-        return res.json({result: 0, error: "Deadline Validation Error", message: "Please according to the format MM(1~12)-DD(1~31)"   });
-        
+      if (split_deadline.length != 3) {
+        return res.json({
+          result: 0,
+          error: "Deadline Validation Error",
+          message: "Please according to the format YYYY(1970~2060)-MM(1~12)-DD(1~31)"
+        });
       }
       if (
-        split_deadline[0] < 1 ||
-        split_deadline[0] > 12 ||
+        split_deadline[0] < 1970 ||
+        split_deadline[0] > 2060 ||
         split_deadline[1] < 1 ||
-        split_deadline[1] > 31
+        split_deadline[1] > 12 ||
+        split_deadline[2] < 1 ||
+        split_deadline[2] > 31
       ) {
-        return res.json({ result: 0, error: "Deadline Validation Error", message: "Please according to the format MM(1~12)-DD(1~31)" });
+        return res.json({
+          result: 0,
+          error: "Deadline Validation Error",
+          message: "Please according to the format YYYY(1970~2060)-MM(1~12)-DD(1~31)"
+        });
       }
-      req.body.deadline = (new Date().getFullYear())+"-"+ req.body.deadline;
     }
 
-    todo.deadline = req.body.deadline
+    todo.deadline = req.body.deadline;
     todo.priority = req.body.priority;
     todo.complete = req.body.complete;
 
     todo.save(function(err) {
       if (err) {
-          console.log(err);
-          
-        return res.json({ result: 0, error: "Todo add fail" ,message: "Please Retry"  });
-        
+        return res.json({
+          result: 0,
+          error: "Todo add fail",
+          message: "Please Retry"
+        });
       }
       res.json({ result: 1 });
     });
@@ -57,7 +70,12 @@ module.exports = function(app, Todo) {
       { _id: ObjectId(req.body._id) },
       { $set: { complete: req.body.complete } },
       function(err, result) {
-        if (err) return res.json({ result: 0, error: "Todo complete fail" ,message: "Please Retry"  });
+        if (err)
+          return res.json({
+            result: 0,
+            error: "Todo complete fail",
+            message: "Please Retry"
+          });
         res.json({ result: 1 });
       }
     );
@@ -65,32 +83,48 @@ module.exports = function(app, Todo) {
 
   app.delete("/todo/delete", function(req, res) {
     Todo.deleteOne({ _id: ObjectId(req.body._id) }, function(err, result) {
-      if (err) return res.json({ result: 0, error: "Todo delete fail" ,message: "Please Retry" });
+      if (err)
+        return res.json({
+          result: 0,
+          error: "Todo delete fail",
+          message: "Please Retry"
+        });
       res.json({ result: 1 });
     });
   });
 
   app.patch("/todo/patch", function(req, res) {
     if (req.body.title == "" || req.body.title == null) {
-        return res.json({ result: 0, error: "Title Valiation Error", message: "Please input title" });
+      return res.json({
+        result: 0,
+        error: "Title Valiation Error",
+        message: "Please input title"
+      });
+    }
+    if (req.body.deadline != "") {
+      var split_deadline = req.body.deadline.split("-");
+      if (split_deadline.length != 3) {
+        return res.json({
+          result: 0,
+          error: "Deadline Validation Error",
+          message: "Please according to the format YYYY(1970~2060)-MM(1~12)-DD(1~31)"
+        });
       }
-      if (req.body.deadline != "") {
-        var split_deadline = req.body.deadline.split("-");
-        if (split_deadline.length != 2) {
-          return res.json({result: 0, error: "Deadline Validation Error", message: "Please according to the format MM(1~12)-DD(1~31)"   });
-          
-        }
-        if (
-          split_deadline[0] < 1 ||
-          split_deadline[0] > 12 ||
-          split_deadline[1] < 1 ||
-          split_deadline[1] > 31
-        ) {
-          return res.json({ result: 0, error: "Deadline Validation Error", message: "Please according to the format MM(1~12)-DD(1~31)" });
-          
-        }
-        req.body.deadline = (new Date().getFullYear())+"-"+ req.body.deadline;
+      if (
+        split_deadline[0] < 1970 ||
+        split_deadline[0] > 2060 ||
+        split_deadline[1] < 1 ||
+        split_deadline[1] > 12 ||
+        split_deadline[2] < 1 ||
+        split_deadline[2] > 31
+      ) {
+        return res.json({
+          result: 0,
+          error: "Deadline Validation Error",
+          message: "Please according to the format YYYY(1970~2060)-MM(1~12)-DD(1~31)"
+        });
       }
+    }
 
     Todo.updateOne(
       { _id: ObjectId(req.body._id) },
@@ -105,7 +139,11 @@ module.exports = function(app, Todo) {
       },
       function(err, result) {
         if (err) {
-          return res.json({ result: 0, error: "Todo update fail" , message: "Please Retry" });
+          return res.json({
+            result: 0,
+            error: "Todo update fail",
+            message: "Please Retry"
+          });
         }
         res.json({ result: 1 });
       }
